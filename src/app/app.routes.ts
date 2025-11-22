1489caf57e55be6af2123f52a4b8e5cdd0567aaf
ENOSISBD\armanul <mdarmanul19@gmail.com>
2025-11-22 15:56:45 +0600

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { LoginComponent } from './components/login-component/login-component';
+import { SignupComponent } from './components/sign-up-component/sign-up-component';
+import { ForgotPasswordComponent } from './components/forgot-password-component/forgot-password-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  // Redirect unknown paths to home
+  { path: '**', redirectTo: '' }
+
+];
